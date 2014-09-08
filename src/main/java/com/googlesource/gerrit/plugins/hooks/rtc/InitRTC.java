48b552c762b3b413a31c66b867b28e4f4b92cb65
Christian Aistleitner <christian@quelltextlich.at>
2014-09-08 15:17:43 +0200

    Update to Gerrit 2.11-SNAPSHOT api
    This commit prepares to be able to build using both Maven, and Buck.
    
    Change-Id: Id5f2eecfd569dd50d1d6a991acddb49436992094

--- src/main/java/com/googlesource/gerrit/plugins/hooks/rtc/InitRTC.java
+++ src/main/java/com/googlesource/gerrit/plugins/hooks/rtc/InitRTC.java
@@ -14,11 +14,11 @@
 package com.googlesource.gerrit.plugins.hooks.rtc;
 
 import com.google.gerrit.extensions.annotations.PluginName;
-import com.google.gerrit.pgm.init.AllProjectsConfig;
-import com.google.gerrit.pgm.init.AllProjectsNameOnInitProvider;
-import com.google.gerrit.pgm.init.InitFlags;
-import com.google.gerrit.pgm.init.Section;
-import com.google.gerrit.pgm.util.ConsoleUI;
+import com.google.gerrit.pgm.init.api.AllProjectsConfig;
+import com.google.gerrit.pgm.init.api.AllProjectsNameOnInitProvider;
+import com.google.gerrit.pgm.init.api.InitFlags;
+import com.google.gerrit.pgm.init.api.Section;
+import com.google.gerrit.pgm.init.api.ConsoleUI;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
